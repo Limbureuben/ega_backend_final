397d6f2b3a7ace37744d847922910734ff1a199d
REUBEN N LIMBU <138168153+Limbureuben@users.noreply.github.com>
2024-09-29 18:21:50 +0300

    Add files via upload
    Te full backend whic are created in ega during field sesion

---
+++ django/app/myapp/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import * # type: ignore
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'password')
+    search_fields = ('username', 'email')
+    
+admin.site.register(Registration, RegistrationAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'firstName', 'lastName')
+    search_fields = ('username', 'email')
+    
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'region', 'phone_number', 'application_date')
+    search_fields = ('username', 'email')
+    
+admin.site.register(Application, ApplicationAdmin)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic', 'published_date', 'text')
+    
+admin.site.register(Message, MessageAdmin)
+
+class EmploymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'job_title', 'employ_status', 'details_month', 'employ_name', 'is_active')
+    
+admin.site.register(Employment, EmploymentAdmin)
+
+class WeatherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'temperature', 'humidity', 'created_at')
+    
+admin.site.register(Weather, WeatherAdmin)
---
+++ django/app/myapp/models.py
@@ -0,0 +1,65 @@
+from pyexpat import model
+from django.db import models
+from datetime import date
+import uuid
+
+# Create your models here.
+
+class Registration(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, default="")
+    email = models.EmailField(max_length=50, default="")
+    password = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.username
+    
+    
+class Application(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, default="null")
+    email = models.EmailField(max_length=255, default="null")
+    region = models.CharField(max_length=255, default="")
+    phone_number = models.CharField(max_length=10, default="0625219727")
+    application_date = models.DateField(default=date.today, null=True, blank=True)
+
+    
+    def __str__(self):
+        return self.username
+    
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    topic = models.CharField(max_length=255, default="null")
+    published_date = models.DateField(default=date.today, null=True)
+    text = models.TextField()
+    
+    
+class Employment(models.Model):
+    # id = models.AutoField(primary_key=True)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    job_title = models.CharField(max_length=55, default="")
+    employ_status = models.CharField(max_length=255, default="")
+    details_month = models.CharField(max_length=255, default="")
+    employ_name = models.CharField(max_length=255, default="")
+    is_active = models.BooleanField(default=True)
+    
+    
+    def __str__(self):
+        return self.employ_status
+    
+
+class Weather(models.Model):
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    
+class Financial(models.Model):
+    # accountname =  models.CharField(max_length=200, nulll=True, default="")
+    accountname = models.CharField(max_length=200, null=True, default="")
+    amount = models.CharField(max_length=200, null=True, blank=True)
+    salary = models.CharField(max_length=200, null=True, default="")
+    username = models.CharField(max_length=200, null=True, blank=True)
+    passport_path = models.CharField(max_length=200, null=True, blank=True)
+    
+class Files(models.Model):
+    file = models.FileField(upload_to="uploads/", null=True, blank=True)
